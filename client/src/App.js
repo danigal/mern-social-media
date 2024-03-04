aa7d24dfd1762b5875fbf5c5e8c8590d73e27492
danigal <56443006+danigal@users.noreply.github.com>
2024-03-04 14:20:43 +0000

    updated socialpedia

--- client/src/App.js
+++ client/src/App.js
@@ -3,13 +3,13 @@ import HomePage from "scenes/homePage";
 import LoginPage from "scenes/loginPage";
 import ProfilePage from "scenes/profilePage";
 import { useMemo } from "react"; // the useMemo hook is used to memoize the result of a function
-import { useSelector } from "react-redux";// the useSelector hook is used to access the state of the store
+import { useSelector } from "react-redux"; // the useSelector hook is used to access the state of the store
 import { CssBaseline, ThemeProvider } from "@mui/material";
 import { createTheme } from "@mui/material/styles";
 import { themeSettings } from "./theme";
 
 function App() {
-  const mode = useSelector((state) => state.mode);// we grab the mode from the state
+  const mode = useSelector((state) => state.mode); // we grab the mode from the state
   const theme = useMemo(() => createTheme(themeSettings(mode)), [mode]);
   const isAuth = Boolean(useSelector((state) => state.token));
 
--- client/src/theme.js
+++ client/src/theme.js
@@ -5,17 +5,17 @@ export const colorTokens = {
   grey: {
     0: "#FFFFFF", // white
     10: "#F6F6F6", // lightest grey
-    50: "#F0F0F0",//  light grey
-    100: "#E0E0E0",// light grey
+    50: "#F0F0F0", //  light grey
+    100: "#E0E0E0", // light grey
     200: "#C2C2C2", // medium grey
-    300: "#A3A3A3",// medium grey
-    400: "#858585",// medium grey
-    500: "#666666",// medium grey
-    600: "#4D4D4D",// medium grey
-    700: "#333333",// dark grey
+    300: "#A3A3A3", // medium grey
+    400: "#858585", // medium grey
+    500: "#666666", // medium grey
+    600: "#4D4D4D", // medium grey
+    700: "#333333", // dark grey
     800: "#1A1A1A", // black
     900: "#0A0A0A", // black
-    1000: "#000000",// black
+    1000: "#000000", // black
   },
   primary: {
     50: "#E6FBFF", // lightest blue
@@ -26,31 +26,34 @@ export const colorTokens = {
     500: "#00D5FA", // main blue
     600: "#00A0BC", // main blue
     700: "#006B7D", // dark blue
-    800: "#00353F",// dark blue
-    900: "#001519",// dark blue
+    800: "#00353F", // dark blue
+    900: "#001519", // dark blue
   },
 };
 
 // mui theme settings
 export const themeSettings = (mode) => {
-  return { // return the themeSettings object
+  return {
+    // return the themeSettings object
     palette: {
       mode: mode,
       ...(mode === "dark" // The ... operator will spread different values to the palette object based on the mode
         ? {
             // pallete values for dark mode
-            primary: {// primary represents the primary color of the design system
-              dark: colorTokens.primary[200],// dark represents the darkest color of the primary color
+            primary: {
+              // primary represents the primary color of the design system
+              dark: colorTokens.primary[200], // dark represents the darkest color of the primary color
               main: colorTokens.primary[500], // main represents the main color of the primary color
-              light: colorTokens.primary[800],// light represents the lightest color of the primary color
+              light: colorTokens.primary[800], // light represents the lightest color of the primary color
             },
-            neutral: {// neutral represents the neutral color of the design system
+            neutral: {
+              // neutral represents the neutral color of the design system
               dark: colorTokens.grey[100],
               main: colorTokens.grey[200],
               mediumMain: colorTokens.grey[300],
               medium: colorTokens.grey[400],
               light: colorTokens.grey[700],
-            },// background represents the background color of the design system
+            }, // background represents the background color of the design system
             background: {
               default: colorTokens.grey[900],
               alt: colorTokens.grey[800], // alt represents the alternative background color of the design system
@@ -76,8 +79,8 @@ export const themeSettings = (mode) => {
             },
           }),
     },
-    typography: { 
-      fontFamily: ["Roboto", "sans-serif"].join(","), 
+    typography: {
+      fontFamily: ["Roboto", "sans-serif"].join(","),
       fontsize: 12,
       h1: {
         fontFamily: ["Roboto", "sans-serif"].join(","),
